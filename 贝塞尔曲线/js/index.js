bc44581e390c66532e203867ea40ca8e686fd980
小键子 <chengjunjian@126.com>
2023-05-26 17:03:36 +0800

    update

--- 贝塞尔曲线/js/index.js
+++ 贝塞尔曲线/js/index.js
@@ -1,21 +1,30 @@
 import { createCanvasContext } from "../../utils/index.js";
 (function () {
   const ctx = createCanvasContext("canvas");
-
+  const controlPoints = [
+    {
+      x: 120,
+      y: 100,
+    },
+    {
+      x: 80,
+      y: 90,
+    },
+  ];
   let _x = 0;
   let _y = 450;
-  let cp1x = 120;
-  let cp1y = 100;
-  let cp2x = 80;
-  let cp2y = 90;
   let x = 450;
   let y = 200;
-  bezierCurveTo(ctx, { _x, _y, cp1x, cp1y, cp2x, cp2y, x, y });
-  ctx.beginPath();
-  setTimeout(() => {
-    bezierCurveTo(ctx, { _x, _y, cp1x: 80, cp1y: 100, cp2x, cp2y, x, y });
-    ctx.beginPath();
-  }, 3000);
+  bezierCurveTo(ctx, {
+    _x,
+    _y,
+    cp1x: controlPoints[0].x,
+    cp1y: controlPoints[0].y,
+    cp2x: controlPoints[1].x,
+    cp2y: controlPoints[1].y,
+    x,
+    y,
+  });
 })();
 
 function bezierCurveTo(ctx, { _x, _y, cp1x, cp1y, cp2x, cp2y, x, y }) {
